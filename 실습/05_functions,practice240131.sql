155229d5b8e1b04603e8bae16bdece6c7b27d04e
BYOUL JUNG <129057067+JungByoul@users.noreply.github.com>
2024-01-31 14:27:02 +0900

    함수 활용실습, 집합 정리

--- 실습/05_functions,practice240131.sql
+++ 실습/05_functions,practice240131.sql
@@ -127,7 +127,69 @@ WHERE customer_id IN (
         WHERE replacement_cost >= 20
     )
 );
+-- 정답. 내 풀이도 가능하다ㅣ 근데 왜 출력값에 차이가 있지
+SELECT LOWER(CONCAT(C.first_name, ', ', C.last_name))
+FROM film F
+JOIN inventory I ON I.film_id = F.film_id
+JOIN rental R ON R.inventory_id = I.inventory_id
+JOIN customer C ON C.customer_id = R.customer_id
+WHERE F.replacement_cost >= 20;
+
+-- 2. film 테이블에서 rating 이 'PG-13'인 영화들에서, description 의 길이가, rating 이 'PG-13'인 영화들의 평
+-- 균 description 길이보다 긴 영화의 제목을 찾으세요
+--  내답. 에러나옴.
+SELECT title
+FROM (SELECT * FROM film a WHERE rating = 'PG-13')
+JOIN film b ON a.film_id = b.film_id
+WHERE a.description IN (
+	SELECT a.description
+	FROM b
+	WHERE AVG(b.description) > a.description
+		and rating = 'PG-13'
+);
+
+-- 정답.ㄷㄷ 간단하네
+SELECT title
+FROM film
+WHERE
+	rating = 'PG-13' AND
+    LENGTH(description) > (
+		SELECT AVG(LENGTH(description))
+		FROM film
+        WHERE rating = 'PG-13'
+		);
+		
+
+-- 아래문제는 시간되면 나중에.
+-- 3.customer 와 rental , inventory, film 테이블을 J0IN하여, 2005년 8월에 대여된 모든 'R' 등급 영화의 제목
+-- 과 해당 영화를 대여한 고객의 이메일을 찾으세요.
+
+-- 4. pavment 테이블에서 최근 30일 이내의 모든 결제를 찾고, 결제 금액을 소수점 둘째 자리에서 반올림하여 출력하세
+-- 요. 이때,각 고객별 총 결제 금액과 평균 결제 금액도 함께 출력해주세요
 
 
 
+-- 이하 집합 차트
+
+-- 1. film 테이블과 film_category 테이블에서 각각중복 없이 film_id 를 조회하는 SQL문을 작성해 보세요.
+SELECT film_id FROM film
+UNION
+SELECT film_id FROM film_category;
+-- - 2.  film 테이블과 film_category 테이블에서 각각 film_id 를 조회하되 중복값도 포함하는 SQL문을 작성해 보세
+SELECT film_id FROM film
+UNION ALL
+SELECT film_id FROM film_category;
+-- 3. film 테이블과 film_category 테이블에서 모두 나오는 film_id 를 조회하는 SQL문을 작성해 보세요.
+SELECT film_id FROM film
+INTERSECT
+SELECT film_id FROM film_category;
+-- 4. film 테이블에는 존재하지만 film_category 테이블에는 존재하지 않는 film_id 를 조회하는 SQL문을 작성해
+-- 보세요.
+SELECT film_id FROM film
+EXCEPT
+SELECT film_id FROM film_category;
+
+
+-- 트랜잭션, COMMIT, ROLLBACK
+-- 면접 때 많이 물어보는 내용. 어라 이거 배웠던건데
 
