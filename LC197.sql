163878e616459d97fe0809575bb3321a7b589a46
BYOUL JUNG <129057067+JungByoul@users.noreply.github.com>
2024-06-20 13:39:10 +0900

    Create LC197.sql

---
+++ LC197.sql
@@ -0,0 +1,16 @@
+USE leetcode;
+
+Create table If Not Exists Weather (id int, recordDate date, temperature int);
+Truncate table Weather;
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10');
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25');
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20');
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30');
+
+SELECT * FROM Weather;
+
+SELECT *
+FROM Weather W1,
+	 (SELECT id, recordDate, DATE_SUB(recordDAte, INTERVAL 1 DAY) Yesterday, temperature FROM Weather) AS W2
+
+;
